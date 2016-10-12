ecf58a70dc139eca3e56fa509cf52a67547ae175
Lionel Lecaque <lionel@taotesting.com>
2016-10-12 10:17:35 +0000

    new version of importCsvTestTaker

--- bin/importCsvTestTaker.php
+++ bin/importCsvTestTaker.php
@@ -1,50 +1,77 @@
 <?php
 
-use oat\taoGroups\models\GroupsService;
-use oat\taoDevTools\helper\NameGenerator;
-use oat\authKeyValue\helpers\DataGeneration;
 use oat\authKeyValue\AuthKeyValueUserService;
+use oat\authKeyValue\helpers\DataGeneration;
+use oat\taoGroups\models\GroupsService;
 
 $parms = $argv;
 array_shift($parms);
 
-if (count($parms) < 2 || count($parms) > 3) {
-    echo 'Usage: '.__FILE__.' TAOROOT CSVFILE [GROUPURI]'.PHP_EOL;
+if (count($parms) < 2 || count($parms) > 4) {
+    echo 'Usage: ' . __FILE__ . ' TAOROOT CSVFILE [LANG] [GROUPURI]' . PHP_EOL;
     die(1);
 }
 
-$root = rtrim(array_shift($parms), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+$root = rtrim(array_shift($parms), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 $csvfile = array_shift($parms);
+$lang = empty($parms) ? null : array_shift($parms);
 $groupUri = empty($parms) ? null : array_shift($parms);
 
-$rawStart = $root.'tao'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'raw_start.php';
+$rawStart = $root . 'tao' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'raw_start.php';
 
 if (!file_exists($rawStart)) {
-    echo 'Tao not found at "'.$rawStart.'"'.PHP_EOL;
+    echo 'Tao not found at "' . $rawStart . '"' . PHP_EOL;
     die(1);
 }
 require_once $rawStart;
 
 if (!file_exists($csvfile)) {
-    echo 'Csv file not found at "'.$csvfile.'"'.PHP_EOL;
+    echo 'Csv file not found at "' . $csvfile . '"' . PHP_EOL;
     die(1);
 }
 
 if (is_null($groupUri)) {
-    $label = 'Group '.NameGenerator::generateRandomString(4);
+    $label = 'Group ' . uniqid();
     $groupClass = new \core_kernel_classes_Class(TAO_GROUP_CLASS);
-    $group = $groupClass->createInstanceWithProperties(array(
-        RDFS_LABEL => $label
-    ));
-    echo 'Group "'.$label.'" created.'.PHP_EOL;
+    $group = $groupClass->createInstanceWithProperties(
+        array(
+            RDFS_LABEL => $label
+        )
+    );
+    echo 'Group "' . $label . '" created.' . PHP_EOL;
     $groupUri = $group->getUri();
 } else {
     $group = new core_kernel_classes_Resource($groupUri);
     if (!$group->exists()) {
-        echo 'Group "'.$groupUri.'" not found.'.PHP_EOL;
+        echo 'Group "' . $groupUri . '" not found.' . PHP_EOL;
         die(1);
     }
 }
+
+
+$persistence = \common_persistence_Manager::getPersistence('default');
+try{
+    $schemaManager = $persistence->getDriver()->getSchemaManager();
+    $schema = $schemaManager->createSchema();
+
+    if(!$schema->hastable('reids')){
+        $fromSchema = clone $schema;
+        $tableResults = $schema->createtable('redis');
+        $tableResults->addOption('engine', 'MyISAM');
+        $tableResults->addColumn('subject', 'string', ['length' => 255]);
+        $tableResults->addColumn('predicate', 'string', ['length' => 255]);
+        $tableResults->addColumn('object', 'string', ['length' => 255]);
+
+        $queries = $persistence->getPlatform()->getMigrateSchemaSql($fromSchema, $schema);
+        foreach ($queries as $query) {
+            $persistence->exec($query);
+        }
+    }
+
+} catch(\Doctrine\DBAL\Schema\SchemaException $e) {
+    \common_Logger::i('Database Schema already up to date.');
+}
+
 
 $expected = array(
     'login' => PROPERTY_USER_LOGIN,
@@ -52,16 +79,15 @@ $expected = array(
 );
 $keys = array_keys($expected);
 $userService = new AuthKeyValueUserService();
-$persistence = \common_persistence_Manager::getPersistence('default');
 
 $row = 1;
-if (($handle = fopen($csvfile, "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+if (($handle = fopen($csvfile, "r")) !== false) {
+    while (($data = fgetcsv($handle, 1000, ";")) !== false) {
         if ($row === 1) {
-            if (json_encode($data) != json_encode(array_keys($expected))) {
-                echo 'Expected data in the format "'.implode('","', $expected).'".'.PHP_EOL;
-                die(1);
-            }
+//            if (json_encode($data) != json_encode(array_keys($expected))) {
+//                echo 'Expected data in the format "'.implode('","', $expected).'".'.PHP_EOL;
+//                die(1);
+//            }
         } else {
             $toAdd = array(
                 GroupsService::PROPERTY_MEMBERS_URI => array($groupUri)
@@ -71,21 +97,32 @@ if (($handle = fopen($csvfile, "r")) !== FALSE) {
             }
 
             // encode password
-            $toAdd[PROPERTY_USER_PASSWORD] = core_kernel_users_Service::getPasswordHash()->encrypt($toAdd[PROPERTY_USER_PASSWORD]);
+            $toAdd[PROPERTY_USER_PASSWORD] = core_kernel_users_Service::getPasswordHash()->encrypt(
+                $toAdd[PROPERTY_USER_PASSWORD]
+            );
 
             if ($userService->getUserData($toAdd[PROPERTY_USER_LOGIN]) != false) {
-                echo 'User "'.$toAdd[PROPERTY_USER_LOGIN].'" already exists.'.PHP_EOL;
-                die(1);
+                echo 'User "' . $toAdd[PROPERTY_USER_LOGIN] . '" already exists.' . PHP_EOL;
+            } else {
+                $userData = DataGeneration::createUser($toAdd, $lang);
+
+                try {
+                    $persistence->insert(
+                        'redis',
+                        array(
+                            'subject' => $userData['uri'],
+                            'predicate' => PROPERTY_USER_LOGIN,
+                            'object' => $userData[PROPERTY_USER_LOGIN]
+                        )
+                    );
+                } catch (PDOException $e) {
+                    echo 'please make sure that called redis exists with subject,predicate,object' . "\n";
+                    echo 'insert as first line : ' . $userData['uri'] . " , " . PROPERTY_USER_LOGIN . " , " . $userData[PROPERTY_USER_LOGIN];
+                    die(1);
+                }
             }
-            $userData = DataGeneration::createUser($toAdd);
-            $persistence->insert(
-                'redis',
-                array(
-                    'subject' => $userData['uri'],
-                    'predicate' => PROPERTY_USER_LOGIN,
-                    'object' => $userData[PROPERTY_USER_LOGIN]
-                )
-            );
+
+
         }
         $row++;
     }
